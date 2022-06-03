b12d6f09af034261e45249f85f09d098602a0967
sakau <sakau@usc.edu>
2022-06-02 19:28:27 -0700

    creates a professional readme file: MVP level

--- Develop/index.js
+++ Develop/index.js
@@ -1,82 +1,76 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { title } = require('process');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the name of your project?'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Describe your project: '
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'What are the steps to install your project?'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'How should your project be used?'
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'Who are the contributors of this project?'
-    },
-    {
-        type: 'checkbox',
-        name: 'license',
-        message: 'Choose a license for your project',
-        choices: ['', '', '', '', '', '', '']
-    },
-    {
-        type: 'input',
-        name: 'license-desc',
-        message: 'What license did you choose and why?'
-    },
-    {
-        type: 'input',
-        name: 'question-sec',
-        message: 'Give directions to reach out to you concerning questions: '
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is your github username?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email?'
-    }
-];
-
 const promptUser = () => {
-    return inquirer.prompt(questions);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Describe your project: '
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps to install your project?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How should your project be used?'
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Choose a license for your project',
+            choices: ['', '', '']
+        },
+        {
+            type: 'input',
+            name: 'licenseDesc',
+            message: 'What license did you choose and why?'
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Give directions to reach out to you concerning questions:'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your github username?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?'
+        }
+    ]);
 }
 
 // TODO: Create a function to write README file
 function writeToFile(data) {
     fs.writeFile('../README.md', data, err => {
         console.log('Error');
+        console.log(data);
         return;
-    })
+    });
 }
 
 function generatePage(readmeData) {
+    console.log(readmeData);
     return `
-#${title}
+# ${readmeData.title}
 
 ## Description
 
-${Description}
+${readmeData.description}
 
 ## Table of Contents
 
@@ -87,29 +81,29 @@ ${Description}
 
 ## Installation
 
-${installation}
+${readmeData.installation}
 
 ## Usage
 
-${usage}
+${readmeData.usage}
 
 ## Questions
 
-${question-sec}
+${readmeData.questions}
 
-Github: ${github}
-Email: ${email}
+Github: ${readmeData.github}
+Email: ${readmeData.email}
 
 ## License
 
-${license-desc}
+${readmeData.licenseDesc}
     `
 }
 
 promptUser()
-.then(readmeData => {
-    return generatePage(readmeData)
-})
-.then(readmeText => {
-    return writeToFile(readmeText)
-})
+    .then(readmeData => {
+        return generatePage(readmeData)
+    })
+    .then(readmeText => {
+        return writeToFile(readmeText)
+    })
--- readme.md
+++ readme.md
@@ -1,25 +1,33 @@
 
-    # Park Buddy
-    
-    ## Purpose
-    Park Buddy was our first project within the Berkely Coding Boot Camp. We were required to utilize two server-sided API's, implement 
-    them into our application, and create a website that both accepts user input to create an unique output. The main functionality of the website
-    is to reccomend any national park in the US by state code while also providing a brief description, contact information, and a map. The is map
-    is fully functional, pinpoints the user's clicked park and allows them to control the viewport. Overall, the project was a great learning 
-    process that allowed our team to dive into bootstrap, javascript, and APIs.
+# a;sldkfjasld;kfj
 
-    ## Build With
-    - HTML
-    - CSS
-    - JavaScript
-    - Bootstrap
-    - Jquery 
-    - National Park Service API
-    - OpenLayers API
+## Description
 
-    ## Website
-    https://magoofy.github.io/project-1/
+as;ldkfj;lsadkjf
 
-    ## Contribution
-    Jolo Cabrera, Afi Akau, Steven Chai, and Nick Kanno
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Questions](#questions)
+- [License](#license)
+
+## Installation
+
+a;sldkfj;lasdkjf
+
+## Usage
+
+lasdkfj;lsadkfj
+
+## Questions
+
+;laskdjf;laskdjf
+
+Github: ;laskdjflasdkj
+Email: al;sdkfjl;asdkjf
+
+## License
+
+alsdkfjl;asdkjf
     
